7991b763c7608b5e36d32f53bf3e717a5f29fd90
Sayadul000005 <Sayadul.haque@northsouth.edu>
2020-11-01 01:17:03 +0600

    Update #2: Update first lab class code.
    Updating first lab class code.

---
+++ Assignments/Lab/Lab-01-October 29 2020 Thursday/MyFirstcode/app/src/main/java/com/nsu/myfirstapplication/cse486/myfirstcode/Sayadul/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nsu.myfirstapplication.cse486.myfirstcode.Sayadul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity onCreate", "Happy");
+    }
+}
---
+++ Assignments/Lab/Lab-01-October 29 2020 Thursday/README.md
@@ -0,0 +1,1 @@
+This is our first lab class code-assignment.
